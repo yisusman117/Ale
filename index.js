cba1f3e0c5576f8ef9a50ec5c76f6d90ba28c5b8
yisusman117 <146797981+yisusman117@users.noreply.github.com>
2024-03-25 12:20:34 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('ya sabia que me preferirias')
+    alert('ESOOOOOOO LA TQM PATRONA')
 });
 
 const noBtn = document.querySelector('#noBtn');
